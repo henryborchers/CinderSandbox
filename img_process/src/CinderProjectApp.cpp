9a656cc526328bdbf62f95b46d03cab718ebf39b
Henry Borchers <henryborchers@yahoo.com>
2018-01-27 21:09:14 -0600

    init commit

--- img_process/src/CinderProjectApp.cpp
+++ img_process/src/CinderProjectApp.cpp
@@ -4,24 +4,84 @@
 
 #include "cinder/gl/gl.h"
 #include "CinderProjectApp.h"
+#include "Resources.h"
 
 using namespace ci;
 using namespace ci::app;
 using namespace std;
 
-void CinderProjectApp::setup()
-{
+void CinderProjectApp::setup() {
+//    Load Parameter interface
+    mParams = params::InterfaceGl("Adjustment params", ivec2(200, 20));
+    mParams.addParam("Brightness", &mBrightness, "min=-0.5 max=0.5 step=0.01 keyIncr== keyDecr=-");
+    mParams.addParam("Contrast", &mContrast, "min=-0 max=10 step=0.01");
+
+//    Load the image
+    mImageInput = loadImage(loadResource(RES_DUMMY));
+    mImageOutput = Surface32f(mImageInput.getWidth(), mImageInput.getHeight(), false);
+
+    mDisplayImageOriginal = gl::Texture::create(mImageInput);
+    equal(1.0f, 2.0f , 3.0);
+//    gl::TextureRef texture = gl::Texture2d::create(loadImage(loadResource(RES_DUMMY)));
+
+//    gl::Texture texture = loadImage(loadResource(RES_DUMMY));
+
+
 }
 
-void CinderProjectApp::mouseDown( MouseEvent event )
-{
+
+void CinderProjectApp::update() {
+    // If no changes, there is no reason to recalculate everything
+    if (equal(mBrightness, mBrightnessPrev) && equal(mContrast, mContrastPrev)){
+        return;
+    }
+
+    Surface32f::ConstIter pixelInIter = mImageInput.getIter();
+    Surface32f::Iter pixelOutIter = mImageOutput.getIter();
+
+    while (pixelInIter.line()){
+        pixelOutIter.line();;
+        while(pixelInIter.pixel()){
+            pixelOutIter.pixel();
+            pixelOutIter.r() = adjustBrightnessContrast(pixelInIter.r());
+            pixelOutIter.g() = adjustBrightnessContrast(pixelInIter.g());
+            pixelOutIter.b() = adjustBrightnessContrast(pixelInIter.b());
+        }
+    }
+
+    // Update the last values so that a new calculation doesn't need to done if nothing has changed
+    mBrightnessPrev = mBrightness;
+    mContrastPrev = mContrast;
+    mDisplayImageOut = gl::Texture::create(mImageOutput);
 }
 
-void CinderProjectApp::update()
-{
+void CinderProjectApp::draw() {
+    gl::clear(Color(0, 0, 0));
+    if (mShowOriginal){
+        gl::draw(mDisplayImageOriginal, getWindowBounds());
+
+    } else{
+        gl::draw(mDisplayImageOut, getWindowBounds());
+
+        mParams.draw();
+    }
 }
 
-void CinderProjectApp::draw()
-{
-    gl::clear( Color( 0, 0, 0 ) );
+float CinderProjectApp::adjustBrightnessContrast(float value) {
+    return (value - 0.5f) * mContrast + 0.5f + mBrightness;
+}
+
+void CinderProjectApp::keyDown(KeyEvent event) {
+    if (event.getChar() == ' ') {
+        mShowOriginal = !mShowOriginal;
+    }
+}
+
+bool CinderProjectApp::equal(float x, float y, float tolerance) {
+//bool CinderProjectApp::equal(float x, float y, float tolerance) {
+    if (fabs(x - y) < tolerance) {
+        return true;
+    } else {
+        return false;
+    }
 }
--- img_process/src/CinderProjectApp.h
+++ img_process/src/CinderProjectApp.h
@@ -1,13 +1,47 @@
 #ifndef CINDER_PROJECT_APP_H
 #define CINDER_PROJECT_APP_H
 #include "cinder/app/App.h"
+#include "cinder/params/Params.h"
+#include "cinder/gl/Texture.h"
 
 class CinderProjectApp : public ci::app::App {
 public:
     void setup() override;
-    void mouseDown( ci::app::MouseEvent event ) override;
     void update() override;
     void draw() override;
+    void keyDown(cinder::app::KeyEvent event) override;
+
+private:
+    bool                    mShowOriginal   = false;
+
+    float                   mBrightness     = 0.0f;
+    float                   mBrightnessPrev = -0.5f;
+
+    float                   mContrast       = 1.0f;
+    float                   mContrastPrev   = 0.0f;
+
+    ci::Surface32f          mImageInput;
+    ci::Surface32f          mImageOutput;
+
+    ci::gl::TextureRef      mDisplayImageOut;
+    ci::gl::TextureRef      mDisplayImageOriginal;
+
+
+    ci::params::InterfaceGl mParams;
+
+    float  adjustBrightnessContrast(float value);
+
+
+    /** Compares 2 values to see if they are within a tolerance
+     *
+     * @param x First value
+     * @param y Second value
+     * @param tolerance How close they the two values have to be if they are to be consider equal
+     * @return If the two values are consider close enough to be equal
+     *
+     */
+    static bool equal (float x, float y, float tolerance = 0.001f);
+
 };
 
 #endif
